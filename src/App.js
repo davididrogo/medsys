81562a9535ab2832aac8bce498b450d9939259a4
eric idrogo <eric.idrogo_consultor@mdp.com.pe>
2025-06-07 12:56:50 -0500

    updating the name of project in landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           MEDSYS ...
         </p>
         <a
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Network Security Capstone Application
         </a>
       </header>
     </div>
